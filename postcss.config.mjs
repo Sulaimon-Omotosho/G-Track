c43148e1b7ae4060b54eda049e53c8a2919398b6
Sulaimon Omotosho <susudef2u@gmail.com>
2025-04-07 14:25:12 +0100

    Chore: Deployment bug

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,18 +1,5 @@
-// import tailwindcss from '@tailwindcss/postcss'
-// // import autoprefixer from 'autoprefixer'
-
-// const config = {
-//   plugins: [tailwindcss, autoprefixer],
-// }
-
-// export default config
-
 const config = {
   plugins: ['@tailwindcss/postcss'],
 }
 
 export default config
-
-// module.exports = {
-//   plugins: ['tailwindcss', 'autoprefixer'],
-// }
